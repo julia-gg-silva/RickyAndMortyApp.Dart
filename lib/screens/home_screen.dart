2deef2d6a2330e246ce4ea1ce6e3c0ba2bc04be0
Julia <juliagabrieligarc@gmail.com>
2025-02-27 21:47:59 -0300

    implementando a navegação

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: dead_code
+
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:rm_app/screens/details_screen.dart';
 import 'package:rm_app/services/rm_api_service.dart';
 import '../models/character.dart';
 
@@ -23,50 +26,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rick and Morty'),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<Character>>(
-        future: _futureCharacters,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Erro: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            final characters = snapshot.data!;
-            return ListView.builder(
-              itemCount: characters.length,
-              itemBuilder: (context, index) {
-                final character = characters[index];
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16)),
-                  child: ListTile(
-                    leading: CachedNetworkImage(
-                      imageUrl: character.image,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) =>
-                          const CircularProgressIndicator(),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
-                    title: Text(character.name),
-                    subtitle: Text('Status: ${character.status}'),
-                  ),
+        appBar: AppBar(
+          title: const Text('Rick and Morty'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: FutureBuilder<List<Character>>(
+            future: _futureCharacters,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Erro: ${snapshot.error}'));
+              } else if (snapshot.hasData) {
+                final characters = snapshot.data!;
+                return ListView.builder(
+                  itemCount: characters.length,
+                  itemBuilder: (context, index) {
+                    final character = characters[index];
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      child: ListTile(
+                        leading: CachedNetworkImage(
+                          imageUrl: character.image,
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                        title: Text(character.name),
+                        subtitle: Text('Status: ${character.status}'),
+                        onTap: () {
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                              builder: (context) => DetailsScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-          }
-          return const Center(child: Text('Nenhum dado disponível'));
-        },
-      ),
-    );
+              }
+              return const Center(child: Text('Nenhum dado disponível'));
+            },
+          ),
+        ));
   }
 }
