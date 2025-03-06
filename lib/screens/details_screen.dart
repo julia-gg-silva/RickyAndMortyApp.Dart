27f91e910e93406db4bdf4df730cc11b0a2e567a
Julia <juliagabrieligarc@gmail.com>
2025-03-06 19:19:32 -0300

    AdicinandoInformacoesPersonagens

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:rm_app/models/character.dart';
 
-class DetailsScreen extends StatefulWidget {
-  const DetailsScreen({super.key});
+class DetailsScreen extends StatelessWidget {
+  final Character character;
 
-  @override
-  State<DetailsScreen> createState() => _DetailsScreenState();
-}
+  const DetailsScreen({super.key, required this.character});
 
-class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Details Character",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25)),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 100),
+          Text(character.name,
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 23)),
+          const SizedBox(height: 25),
+          Image.network(
+            character.image,
+            width: 300,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+           const SizedBox(height: 10),
+          Text('Status: ${character.status}'),
+           const SizedBox(height: 8),
+          Text('Specie: ${character.species}'),
+           const SizedBox(height: 8),
+          Text('Gerder: ${character.gender}'),
+           const SizedBox(height: 8),
+          Text('Origin: ${character.originName}'),
+           const SizedBox(height: 8),
+          Text('Location: ${character.locationName}'),
+        ],
+      )),
+    );
   }
 }
